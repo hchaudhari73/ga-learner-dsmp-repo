05f93e928cbd1541cf719f29d8650458c42309e8
hchaudhari73 <hchaudhari73@users.noreply.github.com>
2020-06-17 09:55:25 +0530

    Code file created.

---
+++ Car-insurance-claim/code.py
@@ -0,0 +1,97 @@
+# --------------
+import pandas as pd 
+from sklearn.model_selection import train_test_split
+
+df = pd.read_csv(path)
+print(df.head())
+print(df.info())
+columns = ["INCOME", "HOME_VAL",'BLUEBOOK','OLDCLAIM','CLM_AMT']
+for col in columns:
+    df[col] = df[col].map(lambda x: str(x).replace("$","").replace(",",""))
+X = df.drop("CLAIM_FLAG", axis=1)
+y = df.CLAIM_FLAG
+X_train,X_test,y_train,y_test = train_test_split(X,y, test_size=0.3, random_state=6)
+
+
+# --------------
+# Code starts here
+for col in columns:
+    X_train[col] = X_train[col].astype(float)
+    X_test[col] = X_test[col].astype(float)
+
+X_train.isnull().any()
+X_test.isnull().any()
+# Code ends here
+
+
+# --------------
+# Code starts here
+X_train.dropna(subset=['YOJ','OCCUPATION'],inplace=True)
+X_test.dropna(subset=['YOJ','OCCUPATION'],inplace=True)
+y_train = y_train[X_train.index]
+y_test = y_test[X_test.index]
+
+
+for col in ["AGE","CAR_AGE","INCOME", "HOME_VAL"]:
+    X_train[col].fillna(X_train[col].mean(), inplace=True)
+    X_test[col].fillna(X_test[col].mean(), inplace=True)
+
+
+# Code ends here
+
+
+# --------------
+from sklearn.preprocessing import LabelEncoder
+columns = ["PARENT1","MSTATUS","GENDER","EDUCATION","OCCUPATION","CAR_USE","CAR_TYPE","RED_CAR","REVOKED"]
+
+# Code starts here
+le = LabelEncoder()
+for col in columns:
+    X_train[col] = le.fit_transform(X_train[col].astype(str))
+    X_test[col] = le.transform(X_test[col].astype(str))
+
+# Code ends here
+
+
+
+# --------------
+from sklearn.metrics import precision_score 
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+
+
+
+# code starts here 
+model = LogisticRegression(random_state=6)
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
+score = accuracy_score(y_test, y_pred)
+print(score)
+# Code ends here
+
+
+# --------------
+from sklearn.preprocessing import StandardScaler
+from imblearn.over_sampling import SMOTE
+
+# code starts here
+smote = SMOTE(random_state=9)
+X_train, y_train = smote.fit_sample(X_train, y_train)
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+
+# Code ends here
+
+
+# --------------
+# Code Starts here
+model = LogisticRegression()
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
+score = accuracy_score(y_test, y_pred)
+print(score)
+# Code ends here
+
+
